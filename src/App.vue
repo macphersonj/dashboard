0760593781eea57a6a31a91118f7c4f07b4fe5a1
macphersonj <james.macpherson@hitfoxgroup.com>
2018-07-27 16:35:41 +1000

    time module

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <div class="container mx-auto px-4">
-      <div class="flex mb-4">
+      <div class="flex">
+        <div class="w-1/2 bg-grey p-10">
+          <clock></clock>
+        </div>
+
+        <div class="w-1/2 bg-grey-light p-10">
+          <weather></weather>
+        </div>
+      </div>
+
+      <div class="flex">
         <div class="w-1/2 bg-grey-light p-10">
           <news></news>
         </div>
         <div class="w-1/2 bg-grey p-10">
-          <weather></weather>
+          <crypto></crypto>
         </div>
       </div>
-      <!-- <crypto></crypto> -->
+
     </div>
   </div>
 </template>
@@ -23,13 +33,15 @@ import '@/assets/styles/main.css'
 import news from './components/News.vue'
 import weather from './components/Weather.vue'
 import crypto from './components/Crypto.vue'
+import clock from './components/Time.vue'
 
 export default {
   name: 'App',
   components: {
     news,
     weather,
-    crypto
+    crypto,
+    clock
   }
 }
 
---
+++ src/components/Time.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex align-center">
+    <p style="font-size: 5em;" v-if="currentTime">
+      {{ currentTime }}
+    </p>
+  </div>
+</template>
+
+
+<script>
+/* eslint-disable */
+
+import moment from 'moment';
+
+export default {
+  name: 'clock',
+  data () {
+    return {
+      currentTime: null
+    }
+  },
+  methods: {
+    updateCurrentTime() {
+      this.currentTime = moment().format('LTS');
+    }
+  },
+  created() {
+    this.currentTime = moment().format('LTS');
+    setInterval(() => this.updateCurrentTime(), 1 * 1000);
+  }
+}
+
+
+</script>
